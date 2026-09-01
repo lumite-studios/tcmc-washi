9fb30e8c11f40ee04251c015d76c0526ce567c61
crotanite <crotanite@lumitestudios.com>
2026-09-01 17:37:35 +0100

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    base: '/tcmc-washi/',
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        }
+    }
 })
